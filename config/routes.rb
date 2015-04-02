89b4a4ff280dc198460d493d0024de951f7307a6
Jianbin Lei <leijianljb@gmail.com>
2015-04-01 21:30:28 -0700

    Ray's modify

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # post 'movie' => 'movie#create'
   get '/search' => 'movie#search'
 
-  resources :movie, only: [:index, :new, :create, :show, :edit]
+  resources :movie, only: [:index, :new, :create, :show, :edit, :update]
 
 
   
